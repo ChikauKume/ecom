fd14bb6275240b1282bce2b7ac268269dfbb1038
ChikauKume <chikau0903@gmail.com>
2021-04-18 21:39:09 +0800

    add cart function

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,9 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('phone_number')->nullable();
+            $table->string('addrArdfZTwN0%0Y6aJJfKJ4ess')->nullable();
+            $table->boolean('is_admin')->default(0);
             $table->rememberToken();
             $table->timestamps();
         });
